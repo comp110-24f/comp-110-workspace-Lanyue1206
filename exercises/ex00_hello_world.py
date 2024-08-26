ef00b10fbb599d9cca53384fc5d8e58e0947f4c6
Lanyue1206 <lanyue@ad.unc.edu>
2024-08-26 15:29:26 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,11 @@
+""""My first exercise in COMP110!"""
+
+__author__ = "730679606"
+
+
+def greet(name: str) -> str:
+    """A welcomeing first function definition"""
+    return "Hello," + name + "!"
+
+
+greet(name="Lanyue")
