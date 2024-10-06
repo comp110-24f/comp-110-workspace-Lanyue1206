0eb8a94f4087672c6059832729b67f4da4af0e01
Lanyue1206 <lanyue@ad.unc.edu>
2024-10-06 18:57:42 -0400

    Draft 2

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,7 +5,8 @@ __author__ = "730679606"
 
 def all(int_list: list[int], number: int) -> bool:
     index = 0
-
+    if len(int_list) == 0:
+        return False
     while index < len(int_list):
         if number == int_list[index]:
             index += 1
@@ -15,21 +16,28 @@ def all(int_list: list[int], number: int) -> bool:
 
 
 def max(input: list[int]) -> int:
-    index = 0  # Prime the loop
 
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
 
+    max: int = input[0]
+    index = 1  # Prime the loop
+
     while index < len(input):
-        if input[index] < input[index + 1]:
-            return input[index + 1]
-        else:
-            return input[index]
-    index += 1
+        if input[index] > max:
+            max = input[index]
+        index += 1
+    return max
 
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
     index = 0
+
+    if len(list1) != len(list2):
+        return False
+
+    if len(list1) == 0 or len(list2) == 0:
+        return False
 
     while index < len(list1):
         if list1[index] == list2[index]:
